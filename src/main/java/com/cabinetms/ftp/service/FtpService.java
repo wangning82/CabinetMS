e1c4b60f9555de717fb36343c94ef9122285ca45
houyi <446902853@qq.com>
2016-06-16 16:03:18 +0800

    FTP服务接口，实现上传下载文件。

---
+++ src/main/java/com/cabinetms/ftp/service/FtpService.java
@@ -0,0 +1,31 @@
+package com.cabinetms.ftp.service;
+
+import java.io.File;
+import java.io.IOException;
+
+public interface FtpService {
+
+    /**
+     * 上传文件
+     * @param path FTP文件路径
+     * @param filename FTP文件名称
+     * @param file 本地文件
+     * @return
+     */
+    boolean upload(String path, String filename, File file) throws IOException;
+
+    /**
+     * 下载文件
+     * @param path FTP文件路径
+     * @param filename FTP文件名称
+     * @param file 本地文件
+     * @return
+     */
+    boolean download(String path, String filename, File file) throws IOException;
+
+    /**
+     * 断开连接
+     * @return
+     */
+    void disconnect();
+}
